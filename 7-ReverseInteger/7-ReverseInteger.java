e34abb0ac5e66abf9d1272c2e3c66c5721a0996f
HARSH KUMAR RATHOR <harshkumarrathor890@gmail.com>
2026-05-06 00:03:35 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.java
@@ -0,0 +1,21 @@
+// Last updated: 5/6/2026, 12:02:56 AM
+class Solution{
+    public int reverse(int n){
+        long sum=0;
+        int sign=1;
+         if(n<0) 
+        {
+            sign=-1;
+            n=-n;
+        }
+        while(n>0){
+            int r=n%10;
+            sum=sum*10+r;
+            n=n/10;
+        }
+        if (sum<Integer.MIN_VALUE||sum>Integer.MAX_VALUE)
+            return 0;
+        sum=sum*sign;
+        return (int)sum;
+    } 
+}
