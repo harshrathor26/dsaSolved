5f9eae452ab974913ee60ed3eec1b86090e7947f
HARSH KUMAR RATHOR <harshkumarrathor890@gmail.com>
2026-05-14 15:44:31 +0530

    Create

---
+++ problem-1778753670408/problem-1778753670408.java
@@ -0,0 +1,14 @@
+// Last updated: 5/14/2026, 3:44:30 PM
+1class Solution {
+2    public int removeElement(int[] nums, int val) {
+3        int index = 0;
+4        for (int i = 0; i < nums.length; i++) {
+5            if (nums[i] != val) {
+6                nums[index] = nums[i];
+7                index++;
+8            }
+9        }
+10        return index;
+11    }
+12}
+13
